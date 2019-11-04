2eae523a095daf8f7abe5f350d892b053fb68463
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-11-04 15:42:54 -0500

    coding jump

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -7,23 +7,53 @@ public class SoyBoyController : MonoBehaviour
 {
     public float speed = 14.0f;
     public float accel = 6.0f;
+    public float jumpSpeed = 8f;
+    public float jumpDurationThreshold = 0.25f;
+    public bool isJumping;    
 
     private Vector2 input; //stores the controllers current input values for x and y at any point in time. negatives are left and down. positives right or up
     private SpriteRenderer sr; 
     private Rigidbody2D rb;
     private Animator animator;
+    private float rayCastLengthCheck = 0.005f;
+    private float width;
+    private float height;
+    private float jumpDuration;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public bool PlayerIsOnGround()
+    {
+        //1 performs raycast directly below center of character
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height),
+                                -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height),
+                                -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height),
+                                -Vector2.up, rayCastLengthCheck);
+
+        //2
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // Update is called once per frame
@@ -34,13 +64,36 @@ public class SoyBoyController : MonoBehaviour
         input.y = Input.GetAxis("Jump");
 
         //2
-        if (input.x > 0f) 
+        if (input.x > 0f)
         {
             sr.flipX = false; //player facing right sprite gets flipped on the x axis
         }
         else if (input.x < 0f)
         {
             sr.flipX = true; //player facing left so sprite not flipped
+        }
+
+        if (input.y >= 1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        if (jumpDuration > jumpDurationThreshold)
+        {
+            input.y = 0f;
         }
     }
 
@@ -65,5 +118,10 @@ public class SoyBoyController : MonoBehaviour
 
         //4
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 }
