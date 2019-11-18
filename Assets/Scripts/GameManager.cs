b490a5b2a8aae13a74fc37d0f48cbd3973f042af
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-11-18 16:03:45 -0500

    PlayerName

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public string playerName;
+
+    private InputField input;
 
     void Awake()
     {
@@ -23,7 +27,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
